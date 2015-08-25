a7bfde2d81418a229159e55072ab3788de729f36
Ryan Weaver <ryan@thatsquality.com>
2015-08-25 02:09:35 -0400

    [tuts-apply-diff] Applying existing diff file for step composition-cacheableshiploader

---
+++ AppBundle/Service/CacheableShipLoader.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Service;
+
+use Doctrine\Common\Cache\Cache;
+
+class CacheableShipLoader
+{
+    /**
+     * @var ShipLoader
+     */
+    private $shipLoader;
+
+    /**
+     * @var Cache
+     */
+    private $cache;
+
+    public function __construct(ShipLoader $shipLoader, Cache $cache)
+    {
+        $this->shipLoader = $shipLoader;
+        $this->cache = $cache;
+    }
+
+    public function getShips()
+    {
+        $key = 'all_ships';
+
+        if ($this->cache->contains($key)) {
+            return $this->cache->fetch($key);
+        }
+
+        $ships = $this->shipLoader->getShips();
+        $this->cache->save($key, $ships);
+
+        return $ships;
+    }
+}
